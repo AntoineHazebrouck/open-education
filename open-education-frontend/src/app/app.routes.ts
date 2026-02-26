90e6bdee0e727e5e0e2f1479ddba69b2c1551030
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-02-26 17:48:13 +0100

    routing ok + navbar ok

--- open-education-frontend/src/app/app.routes.ts
+++ open-education-frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { About } from './components/pages/about/about';
+import { Home } from './components/pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  { path: '**', redirectTo: '' }, // redirect 404 to home
+];
--- open-education-frontend/src/app/components/navbar/navbar.ts
+++ open-education-frontend/src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar, MatButton],
+  imports: [RouterModule, MatToolbar, MatButton],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
