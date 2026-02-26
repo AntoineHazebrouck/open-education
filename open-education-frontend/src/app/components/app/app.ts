cb06927297c306c8afd2c21bbf96ee183e0c0fce
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-02-26 17:37:37 +0100

    navbar done

--- open-education-frontend/src/app/components/app/app.ts
+++ open-education-frontend/src/app/components/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, MatToolbar, MatButton, MatSidenavContainer, MatSidenavContent, MatSidenav],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- open-education-frontend/src/app/components/navbar/navbar.ts
+++ open-education-frontend/src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
+import { MatSidenavContainer } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar, MatButton],
+  imports: [MatToolbar, MatButton, MatSidenavContainer],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
