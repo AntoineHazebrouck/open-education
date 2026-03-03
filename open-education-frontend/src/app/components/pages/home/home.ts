135d21a096bf48b7fac4ca4518d7695fc9fcfea1
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-03-03 16:36:20 +0100

    ongoing

--- open-education-frontend/src/app/components/pages/home/home.ts
+++ open-education-frontend/src/app/components/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { ContentCard } from '../../subcomponents/content-card/content-card';
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule, MatButtonModule, MatGridListModule],
+  imports: [MatCardModule, MatButtonModule, MatGridListModule, ContentCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ open-education-frontend/src/app/components/subcomponents/content-card/content-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-content-card',
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './content-card.html',
+  styleUrl: './content-card.css',
+})
+export class ContentCard {
+  public readonly title = input<string>();
+}
