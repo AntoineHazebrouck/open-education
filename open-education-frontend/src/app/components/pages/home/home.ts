bcbf8cd8f739e72908fc1b97e2a52d7093d02476
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-03-03 17:06:04 +0100

    refactor

--- open-education-frontend/src/app/components/pages/home/home.ts
+++ open-education-frontend/src/app/components/pages/home/home.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ContentCard } from '../../subcomponents/content-card/content-card';
+import { ContentSection } from '../../subcomponents/content-section/content-section';
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule, MatButtonModule, MatGridListModule, ContentCard],
+  imports: [MatCardModule, MatButtonModule, MatGridListModule, ContentCard, ContentSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ open-education-frontend/src/app/components/subcomponents/content-section/content-section.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-content-section',
+  imports: [MatButtonModule],
+  templateUrl: './content-section.html',
+  styleUrl: './content-section.css',
+})
+export class ContentSection {
+  public readonly title = input.required<string>();
+  public readonly subtitle = input.required<string>();
+}
