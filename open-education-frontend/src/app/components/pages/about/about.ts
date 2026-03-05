d4956fbdc4d263263b81004a9e049e52cccf24ea
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2026-03-05 20:39:47 +0100

    moved to <row>

--- open-education-frontend/src/app/components/pages/about/about.ts
+++ open-education-frontend/src/app/components/pages/about/about.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { Row } from '../../subcomponents/row/row';
 
 @Component({
   selector: 'app-about',
-  imports: [MatCardModule, MatDividerModule],
+  imports: [MatCardModule, MatDividerModule, Row],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
